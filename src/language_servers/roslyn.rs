8affb73ba6e461284e4bfcc03c1ccfd4a49e5409
Fabian Freimueller <fabian@freimueller.email>
2025-05-26 15:04:31 +0200

    feat: add download for language server

--- src/language_servers/roslyn.rs
+++ src/language_servers/roslyn.rs
@@ -2,6 +2,8 @@ use std::fs;
 
 use zed_extension_api::{self as zed, settings::LspSettings, LanguageServerId, Result};
 
+const REPO: &str = "SofusA/csharp-language-server";
+
 pub struct Roslyn {
     cached_binary_path: Option<String>,
 }
@@ -28,7 +30,6 @@ impl Roslyn {
             .and_then(|binary_settings| binary_settings.arguments.clone());
 
         if let Some(path) = binary_settings.and_then(|binary_settings| binary_settings.path) {
-            //Roslyn user config
             return Ok(zed::Command {
                 command: path,
                 args: binary_args.unwrap_or_default(),
@@ -45,6 +46,81 @@ impl Roslyn {
                 });
             }
         }
-        Err("Roslyn binary not found".to_string())
+
+        //Not found locally getting remotely
+        zed::set_language_server_installation_status(
+            language_server_id,
+            &zed::LanguageServerInstallationStatus::CheckingForUpdate,
+        );
+        let release = zed::latest_github_release(
+            REPO,
+            zed::GithubReleaseOptions {
+                require_assets: true,
+                pre_release: false,
+            },
+        )?;
+
+        let (platform, arch) = zed::current_platform();
+        let asset_name = format!(
+            "csharp-language-server-{arch}-{os}.{extension}",
+            os = match platform {
+                zed::Os::Mac => "apple-darwin",
+                zed::Os::Linux => "unknown-linux",
+                zed::Os::Windows => "pc-windows-msvc",
+            },
+            arch = match arch {
+                zed::Architecture::Aarch64 => "aarch64",
+                zed::Architecture::X8664 => "x86_64",
+                zed::Architecture::X86 => "unsupported",
+            },
+            extension = match platform {
+                zed::Os::Mac | zed::Os::Linux => "tar.gz",
+                zed::Os::Windows => "zip",
+            }
+        );
+
+        let asset = release
+            .assets
+            .iter()
+            .find(|asset| asset.name == asset_name)
+            .ok_or_else(|| format!("no asset found matching {:?}", asset_name))?;
+
+        let version_dir = format!("roslyn-{}", release.version);
+        let binary_path = format!("{version_dir}/csharp-language-server");
+
+        if !fs::metadata(&binary_path).map_or(false, |stat| stat.is_file()) {
+            zed::set_language_server_installation_status(
+                language_server_id,
+                &zed::LanguageServerInstallationStatus::Downloading,
+            );
+
+            zed::download_file(
+                &asset.download_url,
+                &version_dir,
+                match platform {
+                    zed::Os::Mac | zed::Os::Linux => zed::DownloadedFileType::GzipTar,
+                    zed::Os::Windows => zed::DownloadedFileType::Zip,
+                },
+            )
+            .map_err(|e| format!("failed to download file: {e}"))?;
+
+            zed::make_file_executable(&binary_path)?;
+
+            let entries =
+                fs::read_dir(".").map_err(|e| format!("failed to list working directory {e}"))?;
+            for entry in entries {
+                let entry = entry.map_err(|e| format!("failed to load directory entry {e}"))?;
+                if entry.file_name().to_str() != Some(&version_dir) {
+                    fs::remove_dir_all(entry.path()).ok();
+                }
+            }
+        }
+
+        self.cached_binary_path = Some(binary_path.clone());
+        Ok(zed::Command {
+            command: binary_path,
+            args: binary_args.unwrap_or_default(),
+            env: Default::default(),
+        })
     }
 }
