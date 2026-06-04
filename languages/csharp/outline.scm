811166225903faa2b30946e29bfd02ce2af545c8
MrSubidubi <dev@bahn.sh>
2026-06-04 09:33:27 +0200

    Format Tree-sitter queries

--- languages/csharp/outline.scm
+++ languages/csharp/outline.scm
@@ -1,38 +1,30 @@
 (class_declaration
-    "class" @context
-    name: (identifier) @name
-) @item
+  "class" @context
+  name: (identifier) @name) @item
 
 (constructor_declaration
-    name: (identifier) @name
-) @item
+  name: (identifier) @name) @item
 
 (property_declaration
-    type: (identifier)? @context
-    type: (predefined_type)? @context
-    name: (identifier) @name
-) @item
+  type: (identifier)? @context
+  type: (predefined_type)? @context
+  name: (identifier) @name) @item
 
 (field_declaration
-    (variable_declaration) @context
-) @item
+  (variable_declaration) @context) @item
 
 (method_declaration
-    name: (identifier) @name
-    parameters: (parameter_list) @context
-) @item
+  name: (identifier) @name
+  parameters: (parameter_list) @context) @item
 
 (enum_declaration
-    "enum" @context
-    name: (identifier) @name
-) @item
+  "enum" @context
+  name: (identifier) @name) @item
 
 (namespace_declaration
-    "namespace" @context
-    name: (qualified_name) @name
-) @item
+  "namespace" @context
+  name: (qualified_name) @name) @item
 
 (interface_declaration
-    "interface" @context
-    name: (identifier) @name
-) @item
+  "interface" @context
+  name: (identifier) @name) @item
